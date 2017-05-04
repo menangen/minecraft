20c9cca17366fdb6b87fdb82bf8e6426c55ec670
menangen <menangen@gmail.com>
2017-05-04 22:21:26 +0300

    Original Minecraft

---
+++ minecraft_server/net/minecraft/network/play/server/SPacketChat.java
@@ -0,0 +1,67 @@
+package net.minecraft.network.play.server;
+
+import java.io.IOException;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.INetHandlerPlayClient;
+import net.minecraft.util.text.ITextComponent;
+
+public class SPacketChat implements Packet<INetHandlerPlayClient>
+{
+    private ITextComponent chatComponent;
+    private byte type;
+
+    public SPacketChat()
+    {
+    }
+
+    public SPacketChat(ITextComponent componentIn)
+    {
+        this(componentIn, (byte)1);
+    }
+
+    public SPacketChat(ITextComponent componentIn, byte typeIn)
+    {
+        this.chatComponent = componentIn;
+        this.type = typeIn;
+    }
+
+    /**
+     * Reads the raw packet data from the data stream.
+     */
+    public void readPacketData(PacketBuffer buf) throws IOException
+    {
+        this.chatComponent = buf.readTextComponent();
+        this.type = buf.readByte();
+    }
+
+    /**
+     * Writes the raw packet data to the data stream.
+     */
+    public void writePacketData(PacketBuffer buf) throws IOException
+    {
+        buf.writeTextComponent(this.chatComponent);
+        buf.writeByte(this.type);
+    }
+
+    /**
+     * Passes this Packet on to the NetHandler for processing.
+     */
+    public void processPacket(INetHandlerPlayClient handler)
+    {
+        handler.handleChat(this);
+    }
+
+    /**
+     * This method returns true if the type is SYSTEM or ABOVE_HOTBAR, and false if CHAT
+     */
+    public boolean isSystem()
+    {
+        return this.type == 1 || this.type == 2;
+    }
+
+    public byte func_179841_c()
+    {
+        return this.type;
+    }
+}
